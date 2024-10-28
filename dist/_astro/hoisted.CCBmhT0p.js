b9252148bafbd15450842996e90c66ef065b67aa
Dallas Gale <dallasgale.digital@gmail.com>
2024-10-29 10:10:05 +1100

    wip

--- dist/_astro/hoisted.CCBmhT0p.js
+++
@@ -1,1 +0,0 @@
-import"https://static.elfsight.com/platform/platform.js";const e=document.querySelector(".phone-cta");e?.addEventListener("click",()=>{window.location.href="tel:136736"});
---
+++ dist/_astro/hoisted.CQG2OQQG.js
@@ -0,0 +1,2 @@
+import"https://static.elfsight.com/platform/platform.js";var e=e.$;e.utag_data={};const t=document.createElement("script");t.id="utag";t.src=`https://tags.tiqcdn.com/utag/openaus/sitecore-
+      main/qa/utag.js`;t.async=!0;document.body.appendChild(t);const c=document.querySelector(".phone-cta");c?.addEventListener("click",()=>{window.location.href="tel:136736"});
